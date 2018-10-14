267413b4e854614ff53678be94d77197bb11c79c
samsurabhi <surabhi.mirajkar@gmail.com>
2018-10-14 16:35:34 -0400

    test file

---
+++ json_file_convt.py
@@ -0,0 +1,24 @@
+
+import json
+
+f1 = open("testjson.json", mode = 'w+')
+counter = 0
+with open("test.json") as f:
+	for line in f:
+		if counter == 0 :
+			f1.write('[' + line)
+			counter = 1
+		f1.write(',' + line)
+	f1.write(']')
+		
+with open("testjson.json") as f1:		
+	data = json.load(f1)
+	for n in data[0]:
+		print(n[0])
+with open("testjson.json", mode = 'w') as f1:
+	json.dump(data, f1, indent = 2 )
+
+#print(data[0]['student'])
+#f1.close()
+
+	
